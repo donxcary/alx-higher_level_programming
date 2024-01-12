74abf2f2641525298981deb68468f2823b1978d5
donxcary <donxcary@gmail.com>
2024-01-12 16:47:16 +0000

    Higher Level

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = my_list[:]
-    length = len(new_list)
-    for i in range(0, length):
-        if new_list[i] == search:
-            new_list[i] = replace
-    return(new_list)
+    def s_r_elm(elm):
+        return (elm if elm != search else replace)
+    return list(map(s_r_elm, my_list))
--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    b = dict(a_dictionary)
-    a = list(b)
-    length = len(a)
-    for i in range(0, length):
-        b[a[i]] = b[a[i]] * 2
-    return(b)
+    return {key: value * 2 for key, value in a_dictionary.item()}
