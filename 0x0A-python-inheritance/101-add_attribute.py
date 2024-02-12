53ccda837a2120f5a0d82e49889279100a4a8f1a
donxcary <donxcary@gmail.com>
2024-02-12 16:09:23 +0000

    Higher level

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -3,15 +3,16 @@
 Module defines a function which adds attributes to obj
 """
 
-
-def add_attribute(obj, att, value):
+def add_attribute(obj, attr, value):
     """
-    Check if the object has a __dict__
-         attribute that stores its attribute
+    Check if the object has a __dict__ attribute that stores its attributes
     """
-    if not hasattr(obj, "__dict__"):
-    """
-    Raise a TypeError exception with the message
-    """
+    if hasattr(obj, "__dict__"):
+        """Set the new attribute using setattr"""
+        setattr(obj, attr, value)
+    else:
+        """
+        Raise a TypeError exception with the message
+        """
         raise TypeError("can't add new attribute")
-    setattr(obj, att, value)
+
